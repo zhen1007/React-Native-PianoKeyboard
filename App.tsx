0c1231587b11c8a0d06c3b5647235edff614c091
zhen1007 <172377045+zhen1007@users.noreply.github.com>
2024-09-20 11:33:07 -0400

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,15 +2,29 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import PianoKeyboard from "./PianoKeyboard";
 import { MidiProvider } from "./MidiProvider";
+import { useState } from "react";
 
 export default function App() {
+  const [keyPressed, setKeyPressed] = useState<string[]>([]);
   const onPressKey = (note: string) => {
     console.log("Key pressed:", note);
+    setKeyPressed((prev) => [...prev, note]);
   };
 
   return (
     <MidiProvider>
       <View style={styles.container}>
+        <Text style={styles.title}>React Native Piano KeyBoard</Text>
+        <Text style={styles.piano}>🎹</Text>
+        <Text style={styles.description}>Get inspired from ONLINE PIANIST</Text>
+        <Text style={styles.description}>
+          https://www.onlinepianist.com/virtual-piano
+        </Text>
+        <Text style={styles.description}>
+          Sounds by Tone.js
+        </Text>
+        <View style={{ flex: 1 }} />
+        <Text style={styles.keylog}>Key pressed: {keyPressed.join(", ")}</Text>
         <PianoKeyboard onPressKey={onPressKey} />
         <StatusBar style="auto" />
       </View>
@@ -22,6 +36,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    justifyContent: "center",
   },
+  title: {
+    fontSize: 40,
+    textAlign: "center",
+    marginTop: 80,
+  },
+  piano: {
+    fontSize: 80,
+    textAlign: "center",
+  },
+  description: {
+    fontSize: 18,
+    textAlign: "center",
+    marginTop: 10,
+  },
+  keylog: {
+    paddingHorizontal: 20,
+    marginBottom: 4,
+  }
 });
